628fd76445250e33f4ff82239b473ba2fcf2b1bb
mikacardoso <mikaelabonfim9@gmail.com>
2026-09-17 10:40:43 -0300

    filmes
    adicionei novos filmes

--- filmes.js
+++ filmes.js
@@ -21,7 +21,7 @@ export const filmes = [
 
         
         duracao: "1h 55min",
-        genero: "Drama, Romance",
+        genero: "Drama, ",
         classificacao: "12 anos",
     },
 
@@ -75,6 +75,185 @@ export const filmes = [
         genero: "Drama",
         classificacao: "Livre",
     },
+
+    {
+        id:"FEEL THE BEAT",
+        titulo: "FEEL THE BEAT",
+        imagem: "Imagens/feel.jpg",
+        descricao: "Após um teste dar errado por causa de um mal-entendido com uma jurada, ela perde suas oportunidades e decide voltar relutantemente para sua pequena cidade natal em Wisconsin, indo morar com o pai.",
+        trailer: "hrzrcegbBEc?si=jaNkbP5l9TboIePh",
+
+        duracao: "1h 49min",
+        genero: "Comédia dramática, Família, Dança",
+        classificacao: "Livre",
+    },
+    
+    {
+        id:"MEU ANO EM OXFORD",
+        titulo: "MEU ANO EM OXFORD",
+        imagem: "Imagens/ford.jpg",
+        descricao: "a jovem ambiciosa Anna (Sofia Carson) realiza o sonho de estudar na Universidade de Oxford, mas vê sua vida planejada mudar ao se apaixonar por Jamie (Corey Mylchreest), seu professor de literatura.",
+        trailer: "6GQm-RaMTuQ?si=mcN3LuBWaXVL5Slw",
+
+        duracao: "1h 52min",
+        genero: "Comédia romantica, Drama",
+        classificacao: "12 aanos",
+    },
+    
+    
+    {
+        id:"MENINAS MALVADAS",
+        titulo: "MENINAS MALVADAS",
+        imagem: "Imagens/meninas.jpg",
+        descricao: "Após um teste dar errado por causa de um mal-entendido com uma jurada, ela perde suas oportunidades e decide voltar relutantemente para sua pequena cidade natal em Wisconsin, indo morar com o pai.",
+        trailer: "KfODhuMlaTM?si=T8Kxq1f5opGq9-n2",
+
+        duracao: "1h 37min",
+        genero: "Comédia teen",
+        classificacao: "12 anos",
+    },
+
+    {
+        id:"O DIÁRIO DA PRINCESA",
+        titulo: "O DIÁRIO DA PRINCESA",
+        imagem: "Imagens/prin.jpg",
+        descricao: "Mia, uma adolescente tímida, descobre que é herdeira do trono de Genóvia e precisa aprender a ser uma princesa.",
+        trailer: "bBSZcD1YrtM?si=2l2pHDpUY21DMAHJ",
+
+        duracao: "1h 55min",
+        genero: "Comédia teen",
+        classificacao: "12 anos",
+    },
+
+    {
+        id:"MINHA CULPA",
+        titulo: "MINHA CULPA",
+        imagem: "Imagens/mha.jpg",
+        descricao: "Noah se muda para a mansão do padrasto e vive um romance proibido e perigoso com o meio-irmão, Nick.",
+        trailer: "EkOJWulrkO4?si=VkrE_x-lgrTpFzi-",
+
+        duracao: "1h 57min",
+        genero: "Drama, Romance, Suspense",
+        classificacao: "16 anos",
+    },
+
+    
+    {
+        id:"SUA CULPA",
+        titulo: "SUA CULPA",
+        imagem: "Imagens/sua.jpg",
+        descricao: "Nick e Noah tentam manter o relacionamento vivo na faculdade, enfrentando a oposição dos pais e novos rivais.",
+        trailer: "B2EX6qahZis?si=bGSnsBF5oqKWo_cl",
+
+        duracao: "1h 58min",
+        genero: "Drama, Romance, Suspense",
+        classificacao: "16 anos",
+    },
+
+     
+    {
+        id:"JUSTICEIRAS",
+        titulo: "JUSTICEIRAS",
+        imagem: "Imagens/just.jpg",
+        descricao: "Duas estudantes fazem um acordo secreto para dar o troco e se vingar dos inimigos uma da outra.",
+        trailer: "WsNDWUNT65w?si=v1fJLTjYZ3dRIhXB",
+
+        duracao: "1h 58min",
+        genero: "COMÉDIA TEEN",
+        classificacao: "16 anos",
+    },
+
+    {
+        id:"DOZE É DEMAIS",
+        titulo: "DOZE É DEMAIS",
+        imagem: "Imagens/doze.jpg",
+        descricao: "Um pai precisa cuidar sozinho dos seus 12 filhos bagunceiros enquanto a esposa viaja a trabalho.",
+        trailer: "pR-9gwnZ5bo?si=OTniezbCGhJSCbNT",
+
+        duracao: "1h 38min",
+        genero: "COMÉDIA, Família",
+        classificacao: "Livre",
+    },
+
+    {
+        id:"EXTRAORDINÁRIO",
+        titulo: "EXTRAORDINÁRIO",
+        imagem: "Imagens/extr.jpg",
+        descricao: "Um garoto com deformidade facial frequenta a escola pela primeira vez e ensina todos a praticarem a empatia.",
+        trailer: "7lyJtY_TPvk?si=4nGJxBx-MPeLYGCK",
+
+        duracao: "1h 38min",
+        genero: "Drama, Família",
+        classificacao: "10 anos",
+    },
+
+    {
+        id:"SIMPLESMENTE ACONTECE",
+        titulo: "SIMPLESMENTE ACONTECE",
+        imagem: "Imagens/act.jpg",
+        descricao: "Dois melhores amigos de infância enfrentam desencontros amorosos, gravidez inesperada e mudanças de país enquanto tentam descobrir se foram feitos um para o outro.",
+        trailer: "cXmiics0BTU?si=-EF_PD5p4uzqscGI",
+
+        duracao: "1h 42min",
+        genero: "Drama, Comédia romântica",
+        classificacao: "14 anos",
+    },
+
+    {
+        id:"ASSIM QUE ACABA",
+        titulo: "ASSIM QUE ACABA",
+        imagem: "Imagens/assm.jpg",
+        descricao: "Uma mulher supera uma infância traumática para realizar seus sonhos, mas se vê dividida entre uma paixão intensa, porém tóxica, e o reencontro com seu primeiro amor.",
+        trailer: "wGly-VG1Sn0?si=1QngZgpAKqsx3VS-",
+
+        duracao: "2h 10min",
+        genero: "Drama, Romance",
+        classificacao: "16 anos",
+    },
+
+    {
+        id:"A LISTA DA MINHA VIDA",
+        titulo: "A LISTA DA MINHA VIDA",
+        imagem: "Imagens/alist.jpg",
+        descricao: "Uma jovem descobre que a mãe tem um câncer terminal e embarca em uma jornada para ajudá-la a realizar todos os desejos de uma lista antes de morrer.",
+        trailer: "dkwIWmfpZC0?si=ydHd39I3Nb3RYHkA",
+
+        duracao: "1h 38min",
+        genero: "Drama, Romance",
+        classificacao: "12 anos",
+    },
+
+    {
+        id:"CONTINENCIA DO AMOR",
+        titulo: "CONTINENCIA DO AMOR",
+        imagem: "Imagens/amr.jpg",
+        descricao: "Uma jovem descobre que a mãe tem um câncer terminal e embarca em uma jornada para ajudá-la a realizar todos os desejos de uma lista antes de morrer.",
+        trailer: "2yyzb9RDUgs?si=fKTAXBBQ_PkbBkPd",
+
+        duracao: "2h 02min",
+        genero: "Drama, Romance",
+        classificacao: "14 anos",
+    },
+
+    
+    {
+        id:"O BAD BOY E EU",
+        titulo: "O BAD BOY E EU",
+        imagem: "Imagens/bad.jpg",
+        descricao: "Uma jovem focada nos estudos e com um futuro totalmente planejado vê sua vida virar de cabeça para baixo ao precisar lidar com a chegada de um garoto rebelde e misterioso.",
+        trailer: "D1DdWEmXb04?si=OtFWSycnbFLQvuNM",
+
+        duracao: "1h 35min",
+        genero: "Drama, Romance",
+        classificacao: "12 anos",
+    },
+
+
+
+
+
+
+
 
 ];
 
@@ -152,17 +331,249 @@ export const series = [
         classificacao: "10 anos",
     },
 
-    
     {
-        id:"JOSÉ DO EGITO",
-        titulo: "JOSÉ DO EGITO",
-        imagem: "Imagens/jose.jpg",
-        descricao: "é um personagem bíblico do Livro de Gênesis, filho favorito de Jacó e Raquel.",
-        trailer: "sYvOb1jcqkY?si=nzR4RuwsXgzJw1HX",
+        id:"ELITE",
+        titulo: "ELITE",
+        imagem: "Imagens/elite.jpg",
+        descricao: "Três alunos pobres entram em um colégio de elite espanhol, desencadeando conflitos de classe, escândalos e um assassinato.",
+        trailer: "Oqv81BdRs7w?si=QYxiWWg-4bl08cxZ",
 
-        duracao: "45–50 min por episódio",
-        genero: "Drama, Histórico, Religioso",
-        classificacao: "10 anos",
+        duracao: "45–60 min por episódio",
+        genero: "Drama, Suspense",
+        classificacao: "18 anos",
     },
 
+    
+    {
+        id:"LA CASA DE PAPEL",
+        titulo: "LA CASA DE PAPEL",
+        imagem: "Imagens/lacs.jpg",
+        descricao: "O Professor lidera um grupo de criminosos mascarados no plano perfeito para invadir a Casa da Moeda da Espanha.",
+        trailer: "VY-TETyvlrY?si=cYWy_48QH-2-LeB1",
+
+        duracao: "45–70 min por episódio",
+        genero: "Drama, Suspense, Ação, Drama Policial",
+        classificacao: "16 anos",
+    },
+
+    
+    {
+        id:"PRISON BREAK",
+        titulo: "PRISON BREAK",
+        imagem: "Imagens/pris.jpg",
+        descricao: "Um engenheiro tatua a planta de uma prisão no corpo e se deixa prender para resgatar o irmão inocente do corredor da morte.",
+        trailer: "AL9zLctDJaU?si=xG2y1_Rhyduv8Mwd",
+
+        duracao: "42–45 min por episódio",
+        genero: "Ação, Suspense, Drama Policial",
+        classificacao: "16 anos",
+    },
+
+    {
+        id:"VIS A VIS",
+        titulo: "VIS A VIS",
+        imagem: "Imagens/vis.jpg",
+        descricao: "Uma jovem ingênua vai para uma prisão feminina de alta segurança por golpe do chefe e precisa aprender a sobreviver entre detentas perigosas.",
+        trailer: "N08KRAPzsHU?si=68QNPyOvqfT0OQuN",
+
+        duracao: "40–50 min por episódio",
+        genero: "Drama, Suspense Policial",
+        classificacao: "18 anos",
+    },
+
+    
+    {
+        id:"OS DONOS DO JOGO",
+        titulo: "OS DONOS DO JOGO",
+        imagem: "Imagens/dgj.jpg",
+        descricao: "Um jovem ambicioso tenta dominar o submundo do jogo do bicho no Rio de Janeiro, provocando uma guerra entre famílias de contraventores.",
+        trailer: "2WCWpra0x_I?si=ABH5aDm8tWKvbCEh",
+
+        duracao: "40–60 min por episódio",
+        genero: "Drama, Crime, Policial",
+        classificacao: "16 anos",
+    },
+
+    {
+        id:"FINDING HER EDGE",
+        titulo: "FINDING HER EDGE",
+        imagem: "Imagens/find.jpg",
+        descricao: "Uma patinadora precisa salvar o rinque da família enquanto lida com rivalidades e um triângulo amoroso na patinação artística.",
+        trailer: "ppZ3qgtFOME?si=2383bcWu7RlfzXZA",
+
+        duracao: "45min por episódio",
+        genero: "Drama, Romance, Esporte",
+        classificacao: "12 anos",
+    },
+
+    
+    {
+        id:"GOSSIP GIRL",
+        titulo: "GOSSIP GIRL",
+        imagem: "Imagens/gossp.jpg",
+        descricao: "Um blog anônimo expõe os segredos, dramas e escândalos da elite jovem de Nova York.",
+        trailer: "4OrMd5B4VC4?si=-W00t6T5EdE5KIMm",
+
+        duracao: "40-45 min por episódio",
+        genero: "Drama, Romance",
+        classificacao: "16 anos",
+    },
+
+    {
+        id:"EMILY EM PARIS",
+        titulo: "EMILY EM PARIS",
+        imagem: "Imagens/emi.jpg",
+        descricao: "Uma executiva de marketing americana se muda para Paris a trabalho e tenta conciliar carreira, choques culturais e novos relacionamentos.",
+        trailer: "Ep4mF7Ky2VI?si=RpkspfYavOHXEq1C",
+
+        duracao: "25-40 min por episódio",
+        genero: "Comédia, Drama, Romance",
+        classificacao: "14 anos",
+    },
+
+    {
+        id:"H20",
+        titulo: "H20",
+        imagem: "Imagens/h20.jpg",
+        descricao: "Três garotas ganham poderes mágicos e se transformam em sereias sempre que tocam na água.",
+        trailer: "wqHED34TcwU?si=6i_mvIGG_hDqIuRz",
+
+        duracao: "25min por episódio",
+        genero: "Fantasia, Drama",
+        classificacao: "Livre",
+    },
+
+    
+    {
+        id:"MENTE EXCEPCIONAUMAL",
+        titulo: "MENTE EXCEPCIONAUMAL",
+        imagem: "Imagens/mbt.jpg",
+        descricao: "Uma mãe solteira superdotada deixa de ser faxineira para ajudar a polícia a solucionar crimes complexos.",
+        trailer: "RPOallNnBtk?si=e129-YLHp9zIB2g5",
+
+        duracao: "42min por episódio",
+        genero: "Comédia, Policial, Drama",
+        classificacao: "14 anos",
+    },
+
+    {
+        id:"MINHA VIDA COM A FAMÍLIA WALTER",
+        titulo: "MINHA VIDA COM A FAMÍLIA WALTER",
+        imagem: "Imagens/mvd.jpg",
+        descricao: "Uma garota órfã se muda para o interior com uma grande família e vive um triângulo amoroso com dois irmãos.",
+        trailer: "vUGVhZOFjKg?si=Uj-t8CmSxLKvnG7r",
+
+        duracao: "40-50 min por episódio",
+        genero: "Drama, ROMANCE",
+        classificacao: "12 anos",
+    },
+
+    
+    {
+        id:"PRETTY LITTLE  LIARSPretty",
+        titulo: "PRETTY LITTLE  LIARSPretty",
+        imagem: "Imagens/pll.jpg",
+        descricao: "Quatro amigas passam a ser chantageadas e perseguidas por uma pessoa misteriosa após o desaparecimento da líder do grupo.",
+        trailer: "ItU1BwRqD4I?si=gvFHJ_uxIJpE4Uln",
+
+        duracao: "42-45 min por episódio",
+        genero: "Drama, Suspense, Mistério",
+        classificacao: "14 anos",
+    },
+
+     
+    {
+        id:"A GAROTA DO REMO",
+        titulo: "A GAROTA DO REMO",
+        imagem: "Imagens/remo.jpg",
+        descricao: "Uma promissora atleta se junta ao time masculino de remo de uma escola de elite após um escândalo familiar e enfrenta rivalidades no esporte e no amor.",
+        trailer: "Iic7-CTBz5U?si=Q39BkpYbgCKadmDK",
+
+        duracao: "45 min por episódio",
+        genero: "Drama, Esporte",
+        classificacao: "12 anos",
+    },
+
+    {
+        id:"SINTONIA",
+        titulo: "SINTONIA",
+        imagem: "Imagens/sintonia.jpg",
+        descricao: "Três amigos da mesma periferia paulistana seguem caminhos opostos no funk, no tráfico e na religião.",
+        trailer: "Cn6xcIRk5M8?si=4UwIAY8KfrNKm1_b",
+
+        duracao: "40-55 min por episódio",
+        genero: "Drama, Crime",
+        classificacao: "18 anos",
+    },
+
+    
+    {
+        id:"THE WILDS",
+        titulo: "THE WILDS",
+        imagem: "Imagens/the.jpg",
+        descricao: "Garotas adolescentes tentam sobreviver em uma ilha deserta sem saber que são cobaias de um experimento social.",
+        trailer: "yWldgHy5Dyc?si=PZy8ate7ojJSMRoa",
+
+        duracao: "40-60 min por episódio",
+        genero: "Drama, Suspense, Sobrevivência",
+        classificacao: "16 anos",
+    },
+
+    {
+        id:"O VERÃO QUE MUDOU MINHA VIDA",
+        titulo: "O VERÃO QUE MUDOU MINHA VIDA",
+        imagem: "Imagens/vra.jpg",
+        descricao: "Uma jovem passa o verão na praia e se vê dividida entre o amor de dois irmãos que conhece desde a infância.",
+        trailer: "RayrBWW69Ys?si=2EoD8lyfzNx4dD8Za",
+
+        duracao: "40-50 min por episódio",
+        genero: "Drama, Romance",
+        classificacao: "14 anos",
+    },
+
+
+    {
+        id:"ANNE",
+        titulo: "ANNE",
+        imagem: "Imagens/anne.jpg",
+        descricao: "Uma órfã tagarela e imaginativa é adotada por engano por dois irmãos idosos no Canadá do século XIX",
+        trailer: "S5qJXYNNINo?si=-p1C7ZLb7RbzVfNs",
+
+        duracao: "44-60 min por episódio",
+        genero: "Drama de época, Amadurecimento",
+        classificacao: "12 anos",
+    },
+
+    
+    {
+        id:"BRIDGERTON",
+        titulo: "BRIDGERTON",
+        imagem: "Imagens/bri.jpg",
+        descricao: "Na alta sociedade de Londres do século XIX, os irmãos da família Bridgerton buscam o amor enquanto lidam com escândalos revelados por uma misteriosa e afiada colunista de fofocas.",
+        trailer: "pyi8QAlHR8k?si=MMZP-25k30rJD45U",
+
+        duracao: "45-60 min",
+        genero: "Drama, Romance de época",
+        classificacao: "16 anos",
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
 ];
